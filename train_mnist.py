68b4bea397e2541ac2ae58ebbce7872d7a8e691f
Brandon Walton <BrandonWalton101102@gmail.com>
2025-02-26 00:21:44 -0600

    Train data

--- train_mnist.py
+++ train_mnist.py
@@ -4,6 +4,18 @@ from mnist_load import load_mnist_data
 from sklearn.model_selection import train_test_split
 from mlp.multi_perceptron import MultilayerPerceptron, initialize_layers
 
+def accuracy(y_true, y_pred):
+    """Calculate accuracy by comparing predicted and true labels."""
+    # Convert probabilities to predicted class labels
+    predicted_labels = np.argmax(y_pred, axis=1)
+    true_labels = np.argmax(y_true, axis=1)
+    
+    # Calculate the number of correct predictions
+    correct_predictions = np.sum(predicted_labels == true_labels)
+    
+    # Calculate accuracy as the percentage of correct predictions
+    return correct_predictions / len(y_true)
+
 if __name__ == '__main__':
      print('loading data...')
      (x_train, y_train), (x_test, y_test) = load_mnist_data()
@@ -28,7 +40,7 @@ if __name__ == '__main__':
           hidden_activation=hidden_activation_function,
           output_activation=output_activation_function,
           num_hidden_layers=2,
-          neurons_in_hidden_layer=128, 
+          neurons_in_hidden_layer=32, 
           debug=True
      )
 
@@ -46,6 +58,8 @@ if __name__ == '__main__':
           save_name='mnist_loss.png'
      )
 
+     network_output = multi_p.forward(x=x_test)
 
-
+     acc = accuracy(y_test, network_output)
+     print(f"Accuracy: {acc * 100:.2f}%")
 
